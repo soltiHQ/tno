ed331c490a2131b3bb4a9072bab9f75f6ded234c
mr-chelyshkin <argent-keel-0z@icloud.com>
2025-11-20 18:59:50 +0400

    draft

--- crates/tno-observe/src/lib.rs
+++ crates/tno-observe/src/lib.rs
@@ -1,10 +1,8 @@
 mod logger;
-mod subscriber;
-
 pub use logger::*;
-
-#[cfg(feature = "subscriber")]
-pub use subscriber::*;
-
 #[cfg(feature = "timezone-sync")]
 pub use logger::timezone_sync;
+
+mod subscriber;
+#[cfg(feature = "subscriber")]
+pub use subscriber::*;
