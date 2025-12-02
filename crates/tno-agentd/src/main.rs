57132a6d2b02d5a2d6b9d73300c8db08559ffe4b
Igor Chelyshkin <34075457+mr-chelyshkin@users.noreply.github.com>
2025-12-02 13:50:44 +0400

    update (#8)

--- crates/tno-agentd/src/main.rs
+++ crates/tno-agentd/src/main.rs
@@ -72,7 +72,8 @@ async fn main() -> anyhow::Result<()> {
                 period: 100_000,
             }),
 
-            memory: Some(64 * 1024 * 1024), pids: Some(16),
+            memory: Some(64 * 1024 * 1024),
+            pids: Some(16),
         })
         .with_security(SecurityConfig {
             drop_all_caps: true,
